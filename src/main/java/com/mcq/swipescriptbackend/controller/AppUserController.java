43d8a7b4bbc3067d97fc162ae56f7d399de80cad
Michael <mquinter88@gmail.com>
2025-01-03 23:36:51 -0500

    fix CORS issue

--- src/main/java/com/mcq/swipescriptbackend/controller/AppUserController.java
+++ src/main/java/com/mcq/swipescriptbackend/controller/AppUserController.java
@@ -5,31 +5,29 @@ import com.mcq.swipescriptbackend.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class AppUserController {
 
     private final AppUserRepository appUserRepository;
+
+    @GetMapping
+    public ResponseEntity<List<AppUser>> getAllUsers() {
+        List<AppUser> users = appUserRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<AppUser> getUser(@PathVariable int id) {
         AppUser user = appUserRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
         return ResponseEntity.ok(user);
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<List<AppUser>> getAllUsers() {
-        List<AppUser> users = appUserRepository.findAll();
-        return ResponseEntity.ok(users);
     }
 }
