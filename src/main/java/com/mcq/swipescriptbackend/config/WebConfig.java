70114bc292b26c52b3bd2d29dcafb7e9ce64b6b8
Michael <mquinter88@gmail.com>
2025-01-20 01:52:56 -0500

    update WebConfig

--- src/main/java/com/mcq/swipescriptbackend/config/WebConfig.java
+++ src/main/java/com/mcq/swipescriptbackend/config/WebConfig.java
@@ -2,8 +2,14 @@ package com.mcq.swipescriptbackend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -19,5 +25,24 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(lastActiveInterceptor)
                 .excludePathPatterns("/login", "/register");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+
+                        if (requestedResource.exists() && requestedResource.isReadable()) {
+                            return requestedResource;
+                        }
+
+                        return new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 }
